cd0d9b304e55f1b5e8e9c46053815d49ef80541d
Stevan Blagojcic <sblagojcic@gmail.com>
2017-05-31 15:21:04 +0200

    init commit

---
+++ eObrazovanje/src/main/java/ftn/project/eObrazovanje/EObrazovanjeApplication.java
@@ -0,0 +1,12 @@
+package ftn.project.eObrazovanje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EObrazovanjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EObrazovanjeApplication.class, args);
+	}
+}
